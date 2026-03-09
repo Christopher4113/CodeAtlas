aef295497782f0a8454d7c6e95e902d6a0bc322d
Kanu <kanu@getkanu.com>
2026-03-09 01:01:17 +0000

    chore: IaC changes before deploy (4b5e66)

--- infra/security_groups.tf
+++ infra/security_groups.tf
@@ -69,6 +69,6 @@ resource "aws_vpc_security_group_ingress_rule" "redis_from_ecs" {
 
 resource "aws_vpc_security_group_egress_rule" "redis_all" {
   security_group_id = aws_security_group.redis.id
-  cidr_ipv4         = "0.0.0.0/0"
+  cidr_ipv4         = var.vpc_cidr
   ip_protocol       = "-1"
 }
