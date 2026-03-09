9de5a70d1c9a41c8ba7f1cf075d143a43a0bc471
Kanu <kanu@getkanu.com>
2026-03-09 01:44:35 +0000

    chore: IaC changes before deploy (c90be0)

--- infra/redis.tf
+++ infra/redis.tf
@@ -18,7 +18,8 @@ resource "aws_elasticache_cluster" "redis" {
   parameter_group_name = "default.redis7"
   port                 = 6379
   subnet_group_name    = aws_elasticache_subnet_group.main.name
-  security_group_ids   = [aws_security_group.redis.id]
+  security_group_ids           = [aws_security_group.redis.id]
+  transit_encryption_enabled   = true
 
   tags = {
     Name = "${var.project_name}-redis"
